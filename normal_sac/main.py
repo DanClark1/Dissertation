4df0383e174bc981d1597e17963e47c1b03c0ac5
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-02-05 17:03:40 +0000

    normal sac added

---
+++ normal_sac/main.py
@@ -0,0 +1,281 @@
+import argparse
+import datetime
+import gym
+import numpy as np
+import itertools
+import torch
+from sac import SAC
+from torch.utils.tensorboard import SummaryWriter
+from replay_memory import ReplayMemory
+import random
+import metaworld
+
+def format_obs(o, num_tasks=10):
+        '''
+        Extracts the one-hot encoding from the observation vector,
+        identifies the task and returns the observation vector
+        without the one-hot encoding
+
+        :param o: observation vector
+        :param num_tasks: number of tasks
+        :return: observation vector without the one-hot encoding
+        '''
+        task = np.argmax(o[...,-num_tasks:], axis=-1)
+
+        return o[..., :-num_tasks], task
+
+# Define a custom environment wrapper to include the one-hot task encoding
+class MultiTaskEnv(gym.Env):
+    def __init__(self, env_list, env_names=None):
+        super().__init__()
+        self.num_envs = len(env_list)
+        self.envs = env_list
+        self.env_names = env_names
+        print(self.num_envs)
+        # observation space is max and min of all envs (not sure if this is the same for each anyway?)
+        # and then the one hot vector on the end
+        self.set_max_min_obs()
+        
+        self.observation_space = gym.spaces.Box(
+            low=np.concatenate([self.obs_min, np.zeros(self.num_envs)]),
+            high=np.concatenate([self.obs_max, np.ones(self.num_envs)]),
+            dtype=np.float64
+        )
+
+
+        self.action_space = self.envs[0].action_space # assume all envs have the same action space
+
+        self.current_episode_rewards = 0
+        self.rewards = [[] for i in self.envs]
+        # initialise a random environment
+        self.sample_active_env()
+
+        self.has_reward_been_recorded = False
+
+    def reset(self, **kwargs):
+        '''
+        Resets the current the environment, 
+        then resamples another random environemt
+        and returns the observation state of that'''
+
+
+        if self.has_reward_been_recorded:
+            self.rewards[self.active_index].append(self.current_episode_rewards)
+            self.current_episode_rewards = 0 
+        
+
+        self.active_env.reset(**kwargs)
+        # resample
+        self.sample_active_env()
+        obs = self.active_env.reset(**kwargs)
+
+        self.has_reward_been_recorded = False
+
+        return self._get_obs(obs)
+    
+    def get_env_names(self):
+        '''
+        Returns a list of environment 
+        names if they have been set'''
+        return self.env_names
+    
+    def sample_active_env(self):
+        '''
+        Randomly set an active environment
+        '''
+        self.active_index = random.randint(0, len(self.envs) - 1)
+        self.active_env = self.envs[self.active_index]
+
+    def render(self):
+        self.active_env.render_mode = 'rgb_array'
+        frame = self.active_env.render()
+        self.active_env.render_mode = None
+        return frame
+
+
+    def step(self, action):
+        obs, reward, terminated, truncated, info, *kwargs = self.active_env.step(action)
+        done = terminated or truncated
+
+        self.has_reward_been_recorded = True
+        self.current_episode_rewards += reward
+
+        return self._get_obs(obs), reward, terminated, truncated, *kwargs
+
+    def _get_obs(self, obs):
+        # Create a one-hot encoding of the task ID
+        task_one_hot = np.zeros(self.num_envs)
+        task_one_hot[self.active_index] = 1.0
+
+        # sometimes obs is a tuple??
+        while type(obs) == tuple:
+            obs = obs[0]
+
+        return np.concatenate([obs, task_one_hot])
+        
+
+    def set_max_min_obs(self):
+        '''
+        Finds the maximum and minimum values for the observation spaces
+        '''
+        if self.num_envs != 0:
+            self.obs_max = self.envs[0].observation_space.high
+            self.obs_min = self.envs[0].observation_space.low
+
+            if self.num_envs > 1:
+                for i in range(1, len(self.envs)):
+                    self.obs_max = np.maximum(self.obs_max, self.envs[i].observation_space.high)
+                    self.obs_min = np.minimum(self.obs_min, self.envs[i].observation_space.low)
+
+parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
+parser.add_argument('--env-name', default="",
+                    help='Mujoco Gym environment (default: HalfCheetah-v2)')
+parser.add_argument('--policy', default="Gaussian",
+                    help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
+parser.add_argument('--eval', type=bool, default=True,
+                    help='Evaluates a policy a policy every 10 episode (default: True)')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                    help='discount factor for reward (default: 0.99)')
+parser.add_argument('--tau', type=float, default=0.005, metavar='G',
+                    help='target smoothing coefficient(τ) (default: 0.005)')
+parser.add_argument('--lr', type=float, default=0.0003, metavar='G',
+                    help='learning rate (default: 0.0003)')
+parser.add_argument('--alpha', type=float, default=0.2, metavar='G',
+                    help='Temperature parameter α determines the relative importance of the entropy\
+                            term against the reward (default: 0.2)')
+parser.add_argument('--automatic_entropy_tuning', type=bool, default=False, metavar='G',
+                    help='Automaically adjust α (default: False)')
+parser.add_argument('--seed', type=int, default=123456, metavar='N',
+                    help='random seed (default: 123456)')
+parser.add_argument('--batch_size', type=int, default=256, metavar='N',
+                    help='batch size (default: 256)')
+parser.add_argument('--num_steps', type=int, default=1000001, metavar='N',
+                    help='maximum number of steps (default: 1000000)')
+parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
+                    help='hidden size (default: 256)')
+parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
+                    help='model updates per simulator step (default: 1)')
+parser.add_argument('--start_steps', type=int, default=10000, metavar='N',
+                    help='Steps sampling random actions (default: 10000)')
+parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
+                    help='Value target update per no. of updates per step (default: 1)')
+parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
+                    help='size of replay buffer (default: 10000000)')
+parser.add_argument('--cuda', action="store_true",
+                    help='run on CUDA (default: False)')
+args = parser.parse_args()
+
+# Environment
+# env = NormalizedActions(gym.make(args.env_name))
+mt10 = metaworld.MT10(seed=args.seed) 
+training_envs = []
+names = []
+render = True
+for name, env_cls in mt10.train_classes.items():
+    if render:
+        env = env_cls()
+        render = False
+    else:
+        env = env_cls()
+    task = random.choice([task for task in mt10.train_tasks
+                        if task.env_name == name])
+    env.set_task(task)
+    training_envs.append(env)
+    names.append(name)
+env = MultiTaskEnv(training_envs, env_names=names)
+
+
+
+torch.manual_seed(args.seed)
+np.random.seed(args.seed)
+
+# Agent
+agent = SAC(env.observation_space.shape[0], env.action_space, args)
+
+#Tesnorboard
+writer = SummaryWriter('runs/{}_SAC_{}_{}_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), args.env_name,
+                                                             args.policy, "autotune" if args.automatic_entropy_tuning else ""))
+
+# Memory
+memory = ReplayMemory(args.replay_size, args.seed)
+
+# Training Loop
+total_numsteps = 0
+updates = 0
+
+for i_episode in itertools.count(1):
+    episode_reward = 0
+    episode_steps = 0
+    done = False
+    state = env.reset()
+    while not done:
+        if args.start_steps > total_numsteps:
+            action = env.action_space.sample()  # Sample random action
+        else:
+            action = agent.select_action(state)  # Sample action from policy
+
+        if len(memory) > args.batch_size:
+            # Number of updates per step in environment
+            for i in range(args.updates_per_step):
+                # Update parameters of all the networks
+                critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory, args.batch_size, updates)
+
+                writer.add_scalar('loss/critic_1', critic_1_loss, updates)
+                writer.add_scalar('loss/critic_2', critic_2_loss, updates)
+                writer.add_scalar('loss/policy', policy_loss, updates)
+                writer.add_scalar('loss/entropy_loss', ent_loss, updates)
+                writer.add_scalar('entropy_temprature/alpha', alpha, updates)
+                updates += 1
+
+        next_state, reward, terminated, truncated, *_ = env.step(action) # Step
+        done = terminated or truncated
+        episode_steps += 1
+        total_numsteps += 1
+        episode_reward += reward
+
+        # Ignore the "done" signal if it comes from hitting the time horizon.
+        # (https://github.com/openai/spinningup/blob/master/spinup/algos/sac/sac.py)
+        mask = float(not done)
+
+        memory.push(state, action, reward, next_state, mask) # Append transition to memory
+        
+        state = next_state
+
+    if total_numsteps > args.num_steps:
+        break
+
+    writer.add_scalar('reward/train', episode_reward, i_episode)
+    print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}".format(i_episode, total_numsteps, episode_steps, round(episode_reward, 2)))
+
+    if i_episode % 10 == 0 and args.eval is True:
+        avg_reward = 0.
+        episodes = 10
+        for _  in range(episodes):
+            state = env.reset()
+            episode_reward = 0
+            done = False
+            while not done:
+                action = agent.select_action(state, evaluate=True)
+
+                next_state, reward, terminated, truncated, *_ = env.step(action)
+                done = terminated or truncated
+                episode_reward += reward
+
+                state = next_state
+
+            avg_reward += episode_reward
+        avg_reward /= episodes
+
+
+        writer.add_scalar('avg_reward/test', avg_reward, i_episode)
+
+        print("----------------------------------------")
+        print("Test Episodes: {}, Avg. Reward: {}".format(episodes, round(avg_reward, 2)))
+        print("----------------------------------------")
+
+
+# save model
+agent.save_checkpoint("", ckpt_path="checkpoints/sac_checkpoint_{}_{}".format(args.env_name, ""))
+agent.load_checkpoint(ckpt_path="checkpoints/sac_checkpoint_{}_{}".format(args.env_name, ""))
+env.close()
+
---
+++ normal_sac/replay_memory.py
@@ -0,0 +1,49 @@
+import random
+import numpy as np
+import os
+
+class ReplayMemory:
+    def __init__(self, capacity, seed):
+        random.seed(seed)
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+
+    def push(self, state, action, reward, next_state, done):
+        if type(state) == tuple:
+            print(state)
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(None)
+        self.buffer[self.position] = (state, action, reward, next_state, done)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        batch = random.sample(self.buffer, batch_size)
+        # for i, sample in enumerate(batch):
+            # # print the types first
+            # print(f"Sample {i}: state={type(sample[0])}, action={type(sample[1])}, reward={type(sample[2])}, next_state={type(sample[3])}, done={type(sample[4])}")
+            # print(sample[0])
+            # print(f"Sample {i}: state={sample[0].shape}, action={sample[1].shape}, reward={type(sample[2])}, next_state={sample[3].shape}, done={type(sample[4])}")
+        state, action, reward, next_state, done = map(np.stack, zip(*batch))
+        return state, action, reward, next_state, done
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def save_buffer(self, env_name, suffix="", save_path=None):
+        if not os.path.exists('checkpoints/'):
+            os.makedirs('checkpoints/')
+
+        if save_path is None:
+            save_path = "checkpoints/sac_buffer_{}_{}".format(env_name, suffix)
+        print('Saving buffer to {}'.format(save_path))
+
+        with open(save_path, 'wb') as f:
+            pickle.dump(self.buffer, f)
+
+    def load_buffer(self, save_path):
+        print('Loading buffer from {}'.format(save_path))
+
+        with open(save_path, "rb") as f:
+            self.buffer = pickle.load(f)
+            self.position = len(self.buffer) % self.capacity
