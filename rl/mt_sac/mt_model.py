6d1bba3847af62c83bd4d07879210bc2881244cd
DanClark1 <44947380+DanClark1@users.noreply.github.com>
2025-04-25 17:44:45 +0100

    yuh

--- rl/mt_sac/mt_model.py
+++ rl/mt_sac/mt_model.py
@@ -65,7 +65,7 @@ class MoELayer(nn.Module):
         self.num_tasks = num_tasks
         self.mu = mu
         self.phi = phi
-        self.representation_store_limit = 2000
+        self.representation_store_limit = 200
         self.task_representations = [torch.zeros((self.representation_store_limit, hidden_size)) for _ in range(num_tasks)]
         self.task_representations_count = [0 for _ in range(num_tasks)]
 
